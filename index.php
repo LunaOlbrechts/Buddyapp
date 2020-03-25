e9c99d24e612611976c8b76b1ec89ec103cc4037
lieselottephilips <62293075+lieselottephilips@users.noreply.github.com>
2020-03-25 18:14:23 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <?php include_once("include/nav.inc.php"); ?>
+        <h1 class="col-md-4" >Welkom!</h1>
+</body>
+</html>
