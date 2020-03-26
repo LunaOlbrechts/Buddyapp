b8f40c57df6048db4339887ac6ca35237829e171
Yanis De Maesschalck <yanis705@icloud.com>
2020-03-26 18:03:52 +0100

    Added save() function to User class

--- classes/User.php
+++ classes/User.php
@@ -243,4 +243,70 @@ class User
 
         return $this;
     }
+
+    /**
+     * Get the value of firstName
+     */ 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @return  self
+     */ 
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function save(){
+        // connection
+        $conn = Db::getConnection();
+    
+        // insert query
+        $statement = $conn->prepare("INSERT INTO tl_user (firstName, lastName, email, password) VALUES (:firstName, :lastName, :email, :password) ");
+        
+        
+        $firstname = $this->getFirstName();
+        $lastname = $this->getLastName();
+        $email = $this->getEmail();
+        $password = $this->getPassword();
+    
+        
+        $statement->bindValue(":firstName", $firstname);
+        $statement->bindValue(":lastName", $lastname);
+        $statement->bindValue(":email", $email);
+        $statement->bindValue(":password", $password);
+    
+        $result = $statement->execute();
+    
+        // return result
+        return $result;
+    
+    }
 }
