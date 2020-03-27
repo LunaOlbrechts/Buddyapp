db95f07fee761a1bb8f0e559db0e11655902d056
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-03-27 18:58:14 +0100

    Changed name of table to match the db

--- classes/UserManager.php
+++ classes/UserManager.php
@@ -7,7 +7,7 @@ class UserManager
     public static function saveCompletedProfile(User $user)
     {
         $conn = Db::getConnection();
-        $statement = $conn->prepare("UPDATE tl_users SET city = :location, courseInterests = :courseInterests, schoolYear = :schoolYear, 
+        $statement = $conn->prepare("UPDATE tl_user SET city = :location, courseInterests = :courseInterests, schoolYear = :schoolYear, 
         sportType = :sportType, goingOutType = :goingOutType WHERE id = :id");
 
         $id = $user->getId();
