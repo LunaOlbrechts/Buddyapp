70ff615c05f6a52b1ee3d79c12c4e539521d370b
LowieAelbrecht <Aelbrecht.lowie@gmail.com>
2020-04-03 11:15:14 +0200

    Create buddielist.php

---
+++ buddielist.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buddy app | Buddy list</title>
+</head>
+<body>
+    <?php include_once("include/nav.inc.php"); ?>
+    <h1 class="col-md-4" >This is a list of all people with their buddy!</h1>
+</body>
+</html>
