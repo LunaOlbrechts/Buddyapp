68d2458c48bd08c0cde96467413fabd7553adb87
LowieAelbrecht <Aelbrecht.lowie@gmail.com>
2020-04-03 15:02:42 +0200

    Update buddielist.php

--- buddielist.php
+++ buddielist.php
@@ -1,3 +1,24 @@
+<?php
+    include_once(__DIR__ . "/classes/User.php");
+    include_once(__DIR__ . "/classes/UserManager.php");
+
+    session_start();
+    $id =  $_SESSION["user_id"];
+
+    
+    if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) {
+        $user = new User();
+        $user->setId($id);
+        $users = Usermanager::matches($user);
+        // var_dump($users);            
+    } else {
+        header("Location: login.php");
+    }
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +29,10 @@
 <body>
     <?php include_once("include/nav.inc.php"); ?>
     <h1 class="col-md-4" >This is a list of all people with their buddy!</h1>
+
+    <?php foreach($users as $user): ?>
+        <h2><?php echo $user['email']; ?></h2>
+
+    <?php endforeach; ?>
 </body>
 </html>
