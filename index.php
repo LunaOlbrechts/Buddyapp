b367d7cc8d059d6f4aabb94c4bc3795511c9a57d
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-04-28 11:46:51 +0200

    Added style to the person cards on the index page

--- index.php
+++ index.php
@@ -52,43 +52,52 @@ if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) {
     <?php endif ?>
 
     <?php // echo(json_encode($_SESSION)); ?>
-        <div class="card-text">
-            <p>
-                Er zijn al <?php echo $number_of_users; ?> studenten geregistreerd.
-            </p>
-            <p>
-                Er zijn al <?php echo $number_of_buddy_matches; ?> buddy overeenkomsten gevonden.
-            </p>
+        <div class="card-text amount">
+            <div class="flex-amount">
+                <div class="amount-students">
+                    <p>
+                        Er zijn al <?php echo $number_of_users; ?> studenten geregistreerd
+                    </p>
+                </div>
+                <div class="amount-buddies">
+                    <p>
+                        Er zijn al <?php echo $number_of_buddy_matches; ?> buddy overeenkomsten gevonden
+                    </p>
+                </div>
+            </div>
         </div>
 
     <!--<?php echo(json_encode($_SESSION)); ?>-->
 
-    <div class="profileMatchesByFilters d-flex justify-content-center">
+    <div class="profileMatchesByFilters">
+        <div class="row">
         <?php foreach ($scoresOfMatchedUsers as $matchedUser => $user) : ?>
             <?php if ($user['user_id'] != $_SESSION['user_id']) : ?>
+                <div class="col-sm-6">
                 <div class="card person-card">
-                    <div style="background-image: url(<?php echo htmlspecialchars($user['profilePicture']) ?>); width: 300px; height: 250px; background-size: cover; background-position: center" ;></div>
+                    <div style="background-image: url(<?php echo htmlspecialchars($user['profilePicture']) ?>); height: 250px; background-size: cover; background-position: center" ;></div>
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo htmlspecialchars($user['firstName'] . " " . $user['lastName']) ?></h5>
+                        <a href="http://localhost/files/GitHub/Buddyapp/view.profile.php?id=<?php echo $user['user_id']; ?>" class="collection__item">
+                            <h5 class="card-title"><?php echo htmlspecialchars($user['firstName'] . " " . $user['lastName']) ?></h5>
+                        </a>
                         <p class="card-text">jullie hebben deze kenmerken gemeen:</p>
-                        <?php foreach ($user['matches'] as $match) : ?>
-                            <ul>
-                                <?php if (trim($match) !== '') : ?><li><?php echo $match . ", " ?></li><?php endif ?>
-                            </ul>
-                        <?php endforeach ?>
+                        <ul>
+                            <?php foreach ($user['matches'] as $match) : ?>
+                                    <?php if (trim($match) !== '') : ?><li><?php echo $match . ", " ?></li><?php endif ?>
+                            <?php endforeach ?>
+                        </ul>
 
-                        <form method="POST" enctype="multipart/form-data">
+                        <form method="POST" enctype="multipart/form-data" class="form-person">
                             <input type="hidden" value="<?php echo htmlspecialchars($user['firstName']) ?>" name="receiverName"></input>
                             <input type="hidden" value="<?php echo htmlspecialchars($user['user_id']) ?>" name="receiverId"></input>
-                            <input type="submit" value="Chat" name="chat" class="btn btn-primary"></input>
-                            <a href="http://localhost/files/GitHub/Buddyapp/view.profile.php?id=<?php echo $user['user_id']; ?>" class="collection__item">CLICK me
-                            </a>      
+                            <input type="submit" value="Chat" name="chat" class="btn btn-primary chat"></input>
                         </form>
-
                     </div>
+                </div>
                 </div>
             <?php endif ?>
         <?php endforeach ?>
+        </div>
     </div>          
     <script src="script.js"></script>
 </body>
