c411fdc4bbda23a9391b081e9a90e87fafe0a9cb
Yanis De Maesschalck <yanis705@icloud.com>
2020-05-01 23:29:27 +0200

    Add posts to own profile

--- userFeed.php
+++ userFeed.php
@@ -1,4 +1,25 @@
-<!DOCTYPE html>
+<?php
+
+include_once(__DIR__ . "/classes/Post.php");
+session_start();
+
+$userId = $_SESSION["user_id"];
+
+if (isset($_SESSION["logged_in"]) && $_SESSION["logged_in"]) {
+    if ($_POST['addPost']) {
+
+        $post = New Post();
+        $post->setUserId($userId);
+        $post->setTitle($_POST['title']);
+        $post->setDescription($_POST['description']);
+
+        $post->save();
+    }
+}
+
+$posts = Post::getAllPosts($userId);
+
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -28,11 +49,21 @@
                         <label>Post</label>
                         <textarea name="description" class="form-control" rows="3"><?php echo htmlspecialchars($data['description']) ?></textarea>
                     </div>
-                    <input class="btn btn-primary" type="submit" value="Plaats post" name="updateDetails">
+                    <input class="btn btn-primary" type="submit" value="Plaats post" name="addPost">
                 </form>
             </div>
         </div>
+        <div class="container m-0 p-0">
+            <?php foreach ($posts as $post) : ?>
+                <div class="container mt-3 mb-5 p-0">
+                    <h2><?php echo $post['Title'] ?></h2>
+                    <p><?php echo $post['description'] ?></p>
+                    <p><?php $date = date_create($post['posted_on']);  echo date_format($date, 'd/m/Y') ?></p>
+                </div>
+            <?php endforeach ?>
+        </div>
     </div>
+    
     <?php include_once("include/footer.inc.php"); ?>
 </body>
 
