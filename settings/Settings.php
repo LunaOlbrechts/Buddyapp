0e3741548e04b83e51a688d71a8f4e9270f826b9
LunaOlbrechts <luna.olbrechts@gmail.com>
2020-05-03 22:05:04 +0200

    gitignore; remove settings

---
+++ settings/Settings.php
@@ -0,0 +1,11 @@
+<?php
+
+const SETTINGS = [
+    "db" => [
+        "user"      => "root",
+        "password"  => "",
+        "host"      => "localhost",
+        "port"      => 80,
+        "db"        => "buddy_app"
+    ]
+];
